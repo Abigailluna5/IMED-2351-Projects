4ec2e8f9e541aa64219bc788fb4bcc5b5c659e15
Abigailluna5 <156864097+Abigailluna5@users.noreply.github.com>
2025-09-05 21:44:22 -0500

    update

--- main.js
+++ main.js
@@ -2,3 +2,29 @@
 console.log("Do you think my favorite drink is Dr.Pepper or Coke?");
 
 document.write("Hello Professor!! What's my favorite food?")
+
+// Global variable
+var globalNumber = 10;
+console.log("Global number:", globalNumber);
+
+{
+  // Block variable
+  let blockNumber = 5;
+  console.log("Block number:", blockNumber);
+}
+
+// Mathematical operation
+let sum = globalNumber + 5;
+console.log("Global number + 5 =", sum);
+
+// String variable
+let favoriteFood = "Burgers";
+console.log("My favorite food is:", favoriteFood);
+
+// Object variable
+const person = {
+  name: "Abigail",
+  age: 20,
+  favoriteFood: "Burgers"
+};
+console.log("Person object:", person);
