24872bd1215c3c342da1ac5ab3657c8ecfc48af7
Abigailluna5 <156864097+Abigailluna5@users.noreply.github.com>
2025-09-25 14:48:21 -0500

    commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// Global Var / String value
+var name = "abi";
+
+// Block var/ number value
+{
+    var age = 10;
+}
+
+var score = 60;
+var points = 55;
+console.log(score+points);
+
+
+var green = "duck";
+
+var house="cow";
+
+
+
+
+
